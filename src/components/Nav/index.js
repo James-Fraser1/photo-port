5c76673f063b437bde20a1e08fd476b4dd48b4de
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-04 11:56:59 -0400

    refactoring

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-  const  categories = [
+  const categories = [
     { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
@@ -16,15 +16,15 @@ function Nav() {
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
-              About me
+            <a data-testid="about" href="#about" onClick={() => handleClick()}>
+            About me
             </a>
           </li>
           <li className={"mx-2"}>
@@ -36,7 +36,7 @@ function Nav() {
             categories.map((category) => (
               <li className="mx-1" key={category.name} >
                 <span onClick={() => { handleClick(); }}>
-                 {capitalizeFirstLetter(category.name)}
+                  {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
             ))
